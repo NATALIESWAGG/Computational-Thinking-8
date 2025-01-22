339870748cc460d8150d1e5849104b84ccf3af83
NATALIESWAGG <nataliehartman@seattleacademy.org>
2025-01-22 22:06:30 +0000

    code art

---
+++ Projects/coad_art_intro.py
@@ -0,0 +1,29 @@
+# ###############################################
+# ### SETUP ###
+import turtle
+# ###############################################
+
+t = turtle.Turtle()
+t.penup()
+t.goto(-100, -100)
+t.color("purple")
+t.pendown()
+
+t.forward(72)
+t.left(216)
+t.forward(72)
+t.left(216)
+t.forward(72)
+t.left(216)
+t.forward(72)
+t.left(216)
+t.forward(72)
+
+
+
+
+
+# ###############################################
+# ### ENDING ###
+turtle.exitonclick()
+# ###############################################
